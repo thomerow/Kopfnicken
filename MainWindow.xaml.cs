21c03eba53ad354e6cfd1297538fc7d953653aa6
Thorsten Kummerow <thorsten.kummerow@googlemail.com>
2012-04-01 20:57:06 +0200

    Did not get very far.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,16 @@ namespace Kopfnicken
    /// </summary>
    public partial class MainWindow : Window
    {
-      private KinectSensor _sensor = null;      // Only one Kinect sensor is used in this project
+      struct ColorFrameData
+      {
+         public byte[] Data;           // Data
+         public Point Dim;             // Dimensions
+         public int BytesPerPixel;     // BytesPerPixel
+      }
+
+      private KinectSensor _sensor = null;   // Only one Kinect sensor is used in this project
+      private ColorFrameData _imgCur;        // Current color frame grabbed by the Kinect's camera
+      private Skeleton _skeleton = null;     // Curently tracked skeleton
 
       public MainWindow()
       {
@@ -91,17 +100,34 @@ namespace Kopfnicken
             JitterRadius = 0.05f,
             MaxDeviationRadius = 0.04f
          });
-         _sensor.DepthStream.Enable();
+         _sensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+         _sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
          _sensor.Start();
 
+         // Attach important event handlers
          _sensor.DepthFrameReady += Sensor_DepthFrameReady;
          _sensor.SkeletonFrameReady += Sensor_SkeletonFrameReady;
+         _sensor.ColorFrameReady += Sensor_ColorFrameReady;
+      }
+
+      void Sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+      {
+         ColorImageFrame frame = e.OpenColorImageFrame();
+         if (frame == null) return;
+
+         _imgCur.BytesPerPixel = frame.BytesPerPixel;
+         _imgCur.Data = new byte[frame.BytesPerPixel * frame.Width * frame.Height];
+         _imgCur.Dim.X = frame.Width;
+         _imgCur.Dim.Y = frame.Height;
+         frame.CopyPixelDataTo(_imgCur.Data);
+
+         frame.Dispose();
       }
 
       void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
       {
+         _skeleton = null;
          SkeletonFrame frame = e.OpenSkeletonFrame();
-
          if (frame.SkeletonArrayLength == 0) return;  // No skeleton found
 
          Skeleton[] skeletons = new Skeleton[frame.SkeletonArrayLength];
@@ -111,23 +137,42 @@ namespace Kopfnicken
          {
             if (s.TrackingState == SkeletonTrackingState.Tracked)    // Only process the first skeleton found
             {
+               _skeleton = s;
                TrackHeadAndShoulders(s.Joints);
                break;   // Exit loop after processing one skeleton
             }
          }
 
+         frame.Dispose();
       }
 
       private void TrackHeadAndShoulders(JointCollection joints)
       {
-         Joint jHead = joints[JointType.Head], jShoulderCenter = joints[JointType.ShoulderCenter];
-         if ((jHead.TrackingState != JointTrackingState.Tracked) || (jShoulderCenter.TrackingState != JointTrackingState.Tracked)) return;  // Bail out if head or shouldr center is not tracked correctly
+         Joint jHead = joints[JointType.Head];
+         Joint jShoulderCenter = joints[JointType.ShoulderCenter];
 
+         // Bail out if head or shoulder center is not tracked correctly
+         if ((jHead.TrackingState != JointTrackingState.Tracked) || (jShoulderCenter.TrackingState != JointTrackingState.Tracked)) return;
+
+         ProcessHeadPosition(jShoulderCenter.Position.Z - jHead.Position.Z);
+      }
+
+      private void ProcessHeadPosition(float p)
+      {
+         // ToDo: implement
       }
 
       void Sensor_DepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
       {
+         DepthImageFrame frame = e.OpenDepthImageFrame();
+         if (frame == null) return;
+
+         ColorImagePoint p = frame.MapToColorImagePoint(10, 10, ColorImageFormat.RgbResolution640x480Fps30);
+         //p.
+
          // ToDo: implement
+
+         frame.Dispose();
       }
    }
 }
